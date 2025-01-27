d9aa4974e1899e1fcd050e30a53153bf36e0ab5e
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2025-01-27 09:16:50 +0530

    Feat : Add Some Change

--- admin/src/components/header/addCommit.tsx
+++ admin/src/components/header/addCommit.tsx
@@ -1,5 +1,9 @@
 export default function addCommit() {
   return (
-    <div>addCommit</div>
+    <div>addCommit
+      
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto suscipit natus praesentium ad perferendis cumque amet. Fuga nisi quasi quisquam itaque eaque placeat ipsam perferendis esse ducimus ullam voluptas numquam reprehenderit aliquid natus ad corrupti aspernatur, doloribus atque vero odit sint ipsa neque quod! Veniam assumenda quam aspernatur voluptates.</p>
+    </div>
   )
 }
