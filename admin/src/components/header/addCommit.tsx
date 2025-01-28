d327f550bbc04e812ec3497496da1909fdd0123a
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2025-01-28 08:43:16 +0530

    Feat : add Some Lorem

--- admin/src/components/header/addCommit.tsx
+++ admin/src/components/header/addCommit.tsx
@@ -1,9 +1,13 @@
 export default function addCommit() {
   return (
     <div>addCommit
-      
+
 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto suscipit natus praesentium ad perferendis cumque amet. Fuga nisi quasi quisquam itaque eaque placeat ipsam perferendis esse ducimus ullam voluptas numquam reprehenderit aliquid natus ad corrupti aspernatur, doloribus atque vero odit sint ipsa neque quod! Veniam assumenda quam aspernatur voluptates.</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta reprehenderit, ex ipsam tenetur quaerat exercitationem. Fugit fuga repellat, impedit molestiae autem accusamus error aspernatur vel maiores quis eos nihil voluptate ullam sed quae nesciunt magnam eveniet modi voluptates hic veniam. Quia voluptas facere aliquam! Optio veniam distinctio enim accusantium iste libero consequuntur, laudantium cum suscipit deserunt perferendis voluptatibus ut, consequatur esse eligendi error reiciendis quibusdam! Aspernatur molestias dicta optio maxime maiores harum et ut, eveniet provident corrupti, repellat deleniti veniam vero ipsa? Odio debitis autem deleniti aliquid beatae minus tenetur molestiae, consequatur totam. Eius temporibus, quas doloremque suscipit sint tenetur cumque dolores molestias illum ipsam blanditiis similique fugiat, earum numquam modi sunt? Ipsa, repellat ex quis aut ducimus ad itaque amet incidunt consequatur vel unde, soluta neque quas a eaque assumenda necessitatibus voluptas inventore? Ipsam, quod ducimus? Similique consequuntur itaque doloribus error. Reprehenderit hic tempora eaque soluta. Eligendi temporibus maiores esse, veritatis nesciunt, doloribus quod a, iusto quas nihil quos. Asperiores vero numquam labore accusantium neque molestias provident! Quibusdam ullam quidem optio, ipsum consequuntur quae laboriosam cupiditate dolorem deserunt facere nihil soluta officia maiores placeat excepturi ducimus corporis rem minus quam quos distinctio nemo! Voluptas deleniti ducimus animi inventore corrupti?</p>
+
+
     </div>
   )
 }
