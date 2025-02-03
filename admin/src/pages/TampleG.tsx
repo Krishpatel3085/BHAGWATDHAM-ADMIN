a8250ed58cb0103d4799c5b0178d1c76ea93b529
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2025-02-03 11:34:06 +0530

    Feat : Add Some Cahange

--- admin/src/pages/TampleG.tsx
+++ admin/src/pages/TampleG.tsx
@@ -209,7 +209,7 @@ export default function TampleG() {
                 <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     {uploadedImages.map((img) => (
                         <div key={img._id} className="relative group bg-gray-800 p-3 rounded-lg">
-                            <img src={img.Img} alt={img.ImageName} className="w-full h-48 object-cover rounded-lg" />
+                            <img src={img.Img} alt={img.ImageName} className="w-full h-full object-cover rounded-lg" />
                             <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
                                 <p className="text-white font-semibold">{img.ImageName}</p>
                                 <div className="flex gap-3 mt-2">
